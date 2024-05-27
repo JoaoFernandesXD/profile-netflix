864edcdd23e4a7a2e9207a2c4213f9b893889730
João Fernandes <joaofernandesxd1@icloud.com>
2024-05-26 23:25:04 -0300

    [FEAT] - Ajustando o método de centralizar o container na pagina

--- media/js/index.js
+++ media/js/index.js
@@ -1,6 +1,6 @@
 //Efeito Delay list profile
 document.addEventListener("DOMContentLoaded", function() {
-    var container = document.getElementById('container');
+    var container = document.getElementById('section');
     setTimeout(function() {
         container.classList.remove('hidden');
         container.classList.add('visible'); 
